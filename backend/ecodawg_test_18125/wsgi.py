4c39a9eaea91fdf5aae261ff188957b2460e3944
Crowdbotics <team@crowdbotics.com>
2020-06-15 19:19:46 +0000

    Update models

--- backend/ecodawg_test_18125/wsgi.py
+++ backend/ecodawg_test_18125/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecodawg_test_18125.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecodawg_test_18125.settings")
 
 application = get_wsgi_application()
